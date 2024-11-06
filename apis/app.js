62f18b381ecfc7d5c9c2e0ea226624d8c66a05a2
millenniumSonule <anshumansonuley@gmail.com>
2024-11-06 10:10:44 +0530

    .

--- apis/app.js
+++ apis/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req,res) => {
     res.send("Hello from Express");
 });
-
+ 
 app.listen(3000, ()=>{
     console.log('server is running on http://127.0.0.1:3000')
 });
